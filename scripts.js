889aefeec64b8fbdeaf2bd58beaf66e6dc7d497b
ana <ana@anniecannons.com>
2022-10-29 12:32:10 -0700

    added the last bit of code for the clear button and made comments

--- scripts.js
+++ scripts.js
@@ -4,44 +4,26 @@ $(document).ready(function () {
   let NowMoment = moment().format("MMMM Do YYYY, dddd");
   $("#currentDay").text(NowMoment);
 
-  //  this will store the variables for the attributes in the html for the time
-
-  var nineDiv = $("hours-9");
-  var tenDiv = $("hours-10");
-  var elevenDiv = $("hours-11");
-  var twelveDiv = $("hours-12");
-  var thirteenDiv = $("hours-13");
-  var fourteenDiv = $("hours-14");
-  var fifteenDiv = $("hours-15");
-  var sixteenDiv = $("hours-16");
-  var seventeenDiv = $("hours-17");
-
-  // this is the array to be used to loop threw the  hours to get the right colors to show for the corresponding time
-
-  var hoursArrayInputs = [
-    [09, nineDiv],
-    [10, tenDiv],
-    [11, elevenDiv],
-    [12, twelveDiv],
-    [13, thirteenDiv],
-    [14, fourteenDiv][(15, fifteenDiv)],
-    [16, sixteenDiv],
-    [17, seventeenDiv],
-  ];
-
-  // this  is a loop in jquery changes the colors with when the time is in the past present future
-
   let currentHour = moment().format("HH");
 
   $(".time-div").each(function () {
     var elementHour = $(this).attr("id");
 
     if (elementHour < currentHour) {
-      $(this).removeClass(["present", "future"]).addClass("past");
+      
+      $(this).removeClass("present")
+      $(this).removeClass("future")
+      $(this).addClass("past");
     } else if (elementHour == currentHour) {
-      $(this).removeClass(["past", "future"]).addClass("present");
+      
+      $(this).removeClass("past")
+      $(this).removeClass("future")
+      $(this).addClass("present");
     } else {
-      $(this).removeClass(["past", "present"]).addClass("future");
+      
+      $(this).removeClass("past")
+      $(this).removeClass("present")
+      $(this).addClass("future");
     }
   });
 
@@ -51,7 +33,7 @@ $(document).ready(function () {
     localStorage.setItem(time, plannerText);
   });
 
-  $("#09 .description").val(localStorage.getItem("9"));
+  $("#09 .description").val(localStorage.getItem("09"));
   $("#10 .description").val(localStorage.getItem("10"));
   $("#11 .description").val(localStorage.getItem("11"));
   $("#12 .description").val(localStorage.getItem("12"));
@@ -59,24 +41,23 @@ $(document).ready(function () {
   $("#14 .description").val(localStorage.getItem("14"));
   $("#15 .description").val(localStorage.getItem("15"));
   $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
 
 
 
   // Button function to clear local storage and clear contents
-  $("#clearBtn").click(function (event) {
+  $(".clearBtn").click(function (event) {
     event.preventDefault();
-    $("textarea").val("");
+    $(".description").val("");
     localStorage.clear();
+    console.log('something??????')
   });
-// the saved events are suposed to persist but 2 of myne do not and the clear button dose not work =====================================================================
 
 
   
 });
 
-
-// AS AN employee with a busy schedule
 // I WANT to add important events to a daily planner
 // SO THAT I can manage my time effectively
 // Acceptance Criteria
